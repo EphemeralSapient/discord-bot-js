e1759895a7781d11deb5277807210134efd4e9ef
EphemeralSapient <nowayofway@gmail.com>
2023-04-24 09:51:36 +0530

    Initial commit!

---
+++ commands/fun/ping.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong! Maybe?'),
+    async execute(interaction) {
+        await interaction.deferReply();
+        await wait(1000);
+        await interaction.editReply(`channel id is ${interaction.channel.id}`);
+    },
+};
