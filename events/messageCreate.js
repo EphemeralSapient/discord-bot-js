7174c1a7d1ba7c7e60684ca14394ccdc1a90a441
EphemeralSapient <nowayofway@gmail.com>
2023-05-12 18:35:50 +0530

    Updated

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -21,7 +21,7 @@ module.exports = {
         if (msg.includes(`@<${process.env.clientId}>`)) {
             await message.channel.send(`You called me? | Message : ${withoutMentionsMsg}`);
         }
-        await message.channel.send(msg.includes(`@<${process.env.clientId}>`));
+        await message.channel.send(String(msg.includes(`@<${process.env.clientId}>`)));
 
         // HATE THE F##KING UWU
         if (lmsg.includes("uwu")) await message.channel.send(uwuStop[Math.floor(Math.random() * uwuStop.length)]);
