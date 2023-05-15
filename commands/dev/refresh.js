8b31be1975d60b38b2eca6393769f1bedd5e3404
EphemeralSapient <nowayofway@gmail.com>
2023-05-15 21:27:23 +0530

    Updated

--- commands/dev/refresh.js
+++ commands/dev/refresh.js
@@ -7,7 +7,7 @@ module.exports = {
         .setDescription('Bot will refresh'),
     async execute(interaction) {
         await interaction.editReply("Executing git pull request");
-        exec('git pull', async(error, stdout, stderr) => {
+        exec('rm db.json && git pull', async(error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
                 await interaction.editReply(`Error occurred. \n ${error}`);
