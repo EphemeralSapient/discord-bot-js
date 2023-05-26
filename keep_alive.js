da61934805e7fd7a21bb44433f08bc4925449e8c
EphemeralSapient <nowayofway@gmail.com>
2023-05-26 23:06:59 +0530

    Updated

--- keep_alive.js
+++ keep_alive.js
@@ -6,20 +6,21 @@ http.createServer(function(req, res) {
     var filePath;
     if (req.url === '/') {
         filePath = './html/welcome.html';
-    } else if (req.url == '/gitpush') {
-        // Execute the 'git push' command
-        exec('git push', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
     } else {
+        if (req.url == '/gitpush') {
+            // Execute the 'git push' command
+            exec('git push', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
+        }
         console.log(req.url)
         filePath = './html/notfound.html';
     }
