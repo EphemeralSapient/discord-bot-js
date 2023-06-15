02927a406c795e95de96b34c6c05fd278b757ada
EphemeralSapient <nowayofway@gmail.com>
2023-06-15 17:18:07 +0530

    chatgpt

--- keep_alive.js
+++ keep_alive.js
@@ -3,9 +3,51 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
-    var filePath;
+    var filePath = null;
+    console.log(req.method + " incoming.")
     if (req.url === '/') {
         filePath = './html/welcome.html';
+    } else if(req.url == "/course") {
+        let body = '';
+
+        req.on('data', (chunk) => {
+          // Accumulate the data chunks
+          body += chunk;
+        });
+    
+        req.on('end', async () => {
+          // Process the complete request body
+          console.log("course convertion request. \n" + body);
+            
+          var part1 = (await global.chatGpt.sendMessage(body + `\nGet me json format of this data for the following keys
+
+          'code' : string
+          'title' : string
+          'title' : string 
+          'LTPC' : list of numbers
+          'syllabus_topic' : List of String 
+          'syllabus_credits' : List of numbers 
+          'syllabus_subtopic' : List of String`)).text;
+          var part2 = (await global.chatGpt.sendMessage(body + `\nnGet me json format of this data for the following keys
+
+          'textbook' : List of String
+          'reference' : List of string 
+          'objectives' : List of String
+            'outcomes' : List of String`)).text;
+
+          try {
+            part1 = JSON.parse(part1)
+            part2 = JSON.parse(part2)
+
+            const combinedObj = Object.assign({}, pat2, part1);
+
+            res.write(JSON.stringify(combinedObj))
+          } catch(e) {
+            res.write("fail");
+          }
+
+          res.end();
+        });
     } else {
         if (req.url == '/gitpush') {
             // Execute the 'git pull' command
@@ -22,14 +64,17 @@ http.createServer(function(req, res) {
         console.log(req.url)
         filePath = './html/notfound.html';
     }
-    fs.readFile(filePath, function(err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.write('File not found');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-        }
-        res.end();
-    });
+    
+    if(filePath != null){
+        fs.readFile(filePath, function(err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.write('File not found');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+            }
+            res.end();
+        });
+    }
 }).listen(8080);
